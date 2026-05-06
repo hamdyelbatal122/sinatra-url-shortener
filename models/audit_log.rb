92fc0f606ac2f26690312b6c5edfe6d012b1e66c
Hamdy Elbatal <elbatalhamdy77@gmail.com>
2026-05-06 21:50:26 +0300

    Update audit_log.rb

--- models/audit_log.rb
+++ models/audit_log.rb
@@ -6,7 +6,8 @@ migration 'create audit_logs' do
     String :action, null: false
     String :entity_type, null: false
     Integer :entity_id, null: false
-
+    Integer :user_id, null: true
+    String :details, null: true
     DateTime :created_at
     index :entity_type
     index :user_id
